4655b515516b7c38605641680f6d17d2e0bf580c
mi <mi@DESKTOP-OVUF872>
2023-10-27 02:15:22 +0100

    edit profile, upload and  display new image

--- basic/resources/views/admin/admin_profile_edit.blade.php
+++ basic/resources/views/admin/admin_profile_edit.blade.php
@@ -1,5 +1,6 @@
 @extends('admin.admin_master')
 @section('admin')
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 
 <div class="page-content">
     <div class="container-fluid">
@@ -34,9 +35,9 @@
                                 </div>
                             </div>
                             <div class="row mb-3">
-                                <label for="profile_image" class="col-sm-2 col-form-label"></label>
+                                <label for="display_image" class="col-sm-2 col-form-label"></label>
                                 <div class="col-sm-10">
-                                    <img class="rounded avatar-lg mt-3" src=" {{ asset('backend/assets/images/small/img-5.jpg') }}" alt="Card image cap">
+                                    <img class="rounded avatar-lg mt-3" id="display_image" src=" {{ asset('backend/assets/images/small/img-5.jpg') }}" alt="Card image cap">
                                 </div>
                             </div>
                             <input type="submit" value="Update Profile" class="btn btn-info waves-effect waves-light">
@@ -48,4 +49,17 @@
         </div>
     </div>
 </div>
+
+<script type="text/javascript">
+    $(document).ready(function(){
+        $('#profile_image').change(function(e){
+            var reader = new FileReader();
+            reader.onload = function(e){
+                $('#display_image').attr('src',e.target.result);
+            }
+            reader.readAsDataURL(e.target.files['0']);
+        });
+    });
+</script>
+
 @endsection
