02360b9b4bbd31fc020961b98e27c9e6d12da424
mi <mi@DESKTOP-OVUF872>
2023-11-06 04:13:55 +0000

    Blog model and miration created

---
+++ basic/database/migrations/2023_11_06_005437_create_blogs_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('blog_category_id');
+            $table->foreign('blog_category_id')->references('id')->on('blog_categories');
+            $table->string('title')->nullable();
+            $table->string('image')->nullable();
+            $table->string('tags')->nullable();
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('blogs');
+    }
+};
