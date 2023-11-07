9adcaacf809e20eae68ff1c1ae5fdc418f3eb9e9
mi <mi@DESKTOP-OVUF872>
2023-11-07 05:07:53 +0000

    frontend footer display data

--- basic/resources/views/frontend/body/footer.blade.php
+++ basic/resources/views/frontend/body/footer.blade.php
@@ -1,3 +1,6 @@
+@php
+    $footer = App\Models\Footer::find(1);
+@endphp
 <footer class="footer">
     <div class="container">
         <div class="row justify-content-between">
@@ -5,12 +8,10 @@
                 <div class="footer__widget">
                     <div class="fw-title">
                         <h5 class="sub-title">Contact us</h5>
-                        <h4 class="title">+81383 766 284</h4>
+                        <h4 class="title">{{ $footer->phone }}</h4>
                     </div>
                     <div class="footer__widget__text">
-                        <p>There are many variations of passages of lorem ipsum
-                        available but the majority have suffered alteration
-                        in some form is also here.</p>
+                        <p>{{ $footer->description }}</p>
                     </div>
                 </div>
             </div>
@@ -18,11 +19,10 @@
                 <div class="footer__widget">
                     <div class="fw-title">
                         <h5 class="sub-title">my address</h5>
-                        <h4 class="title">AUSTRALIA</h4>
                     </div>
                     <div class="footer__widget__address">
-                        <p>Level 13, 2 Elizabeth Steereyt set <br> Melbourne, Victoria 3000</p>
-                        <a href="mailto:noreply@envato.com" class="mail">noreply@envato.com</a>
+                        <p><h5 class="sub-title">{{ $footer->address }}</h5></p>
+                        <a href="mailto:noreply@envato.com" class="mail">{{ $footer->email }}</a>
                     </div>
                 </div>
             </div>
@@ -33,13 +33,9 @@
                         <h4 class="title">socially connect</h4>
                     </div>
                     <div class="footer__widget__social">
-                        <p>Lorem ipsum dolor sit amet enim. <br> Etiam ullamcorper.</p>
                         <ul class="footer__social__list">
-                            <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-                            <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-                            <li><a href="#"><i class="fab fa-behance"></i></a></li>
-                            <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
-                            <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                            <li><a href="{{ url($footer->facebook) }}"><i class="fab fa-facebook-f"></i></a></li>
+                            <li><a href="{{ url($footer->twitter) }}"><i class="fab fa-twitter"></i></a></li>
                         </ul>
                     </div>
                 </div>
@@ -49,7 +45,7 @@
             <div class="row">
                 <div class="col-12">
                     <div class="copyright__text text-center">
-                        <p>Copyright @ Theme_Pure 2021 All right Reserved</p>
+                        <p>{{ $footer->copy_right }}</p>
                     </div>
                 </div>
             </div>
